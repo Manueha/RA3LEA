3a82a162942e8b0468d119a56f359e2424ebb572
Manueha <manel.dominguez@institutvidreres.cat>
2024-11-20 12:58:57 +0100

    hola

--- .idea/Practiques/JSLogicaManelDominguez.js
+++ .idea/Practiques/JSLogicaManelDominguez.js
@@ -11,14 +11,20 @@
 // }
 
 //2
-//     let x = 9
-//     let y = 50
-//
-//     function rang(x,y){
-//         x || y >=10 && x || y <= 50 ? console.log("Un numero esta en el rang") : console.log("Cap numero esta en el rang")
-//     }
-//
-//     rang(x,y)
+    let x = 40
+    let y = 50
+
+    function rang(x,y){
+    //faig una ternaria y un dels resultats de la ternaria es una altra ternaria que revisa el valor de y
+        x >= 10 && x <= 50 ? y >= 10 && y <= 50
+                ? console.log("Els numeros " + x + " " +  y + " Estan en el rang")
+                : console.log("nomes el numero  " + x + " Esta en el rang")
+                : y >= 10 && y <= 50
+                    ? console.log("Nomes el numero  " + y + " Esta en el rang")
+                    : console.log( "Cap dels numeros esta en el rang")
+    }
+
+    rang(x,y)
 
 //3
 
@@ -71,59 +77,156 @@
 
 //5
 
-    let x = 15;
-    let suma = 0;
-    let n = 0;
-
-    for (let i = 1; suma + i <= x; i++) {
-        suma += i;
-        n = i;
-    }
-
-    console.log("El maxim numero es : " + n);
-
+//     let x = 15;
+//     let suma = 0;
+//     let n = 0;
+//
+//     for (let i = 1; suma + i <= x; i++) {
+//         suma += i;
+//         n = i;
+//     }
+//
+//     console.log("El maxim numero es : " + n);
+//
 //6
 
 
-    // let hola1 = "bon dia 20"
-    // let  hola2 = "mal dia 20"
-    //
-    // function sumarStrings(string1, string2){
-    //     //utilitzo match per a fer la expressio regular que em retorna el numero dins de un array que transformo a integer utilitzant el parse
-    //     let numholaa = parseInt(string1.match(/\d+/))
-    //     let numholab = parseInt(string2.match(/\d+/))
-    //     return numholaa + numholab
-    // }
-    // console.log(sumarStrings(hola1,hola2))
+let text = "1eok1i90jei19e2ºjfe2jq13nfu21eº2bhiefi12jo239i0fjog23u90ifrjih2u9ieiwjio"
 
+function sumarStrings (text){
+    let suma = 0
+    for (let i = 0; i < text.length; i++) {
+        let caracter = text[i]
+        if (!isNaN(caracter) && caracter !== ' '){
+            suma += parseInt(caracter)
+        }
+    }
+    return suma
+}
+
+console.log(sumarStrings(text))
 
 //7
 
-    enter1 = 49
-    enter2 = 50
-    enter3 = 30
+    // enter1 = 49
+    // enter2 = 50
+    // enter3 = 30
+    //
+    // function ordenarMajorMenor(a,b,c){
+    //     if (a > b && a > c){
+    //         if (b > c){
+    //             return console.log(a + " "+ b + " " + c)
+    //         }else {
+    //            return console.log(a + " " +  c + " "+ b)
+    //         }
+    //     }else if (b > a && b > c){
+    //         if (a > c){
+    //             return console.log(b + " " + a + " " + c )
+    //         }else {
+    //             return console.log(b + " " + c + " " + a)
+    //         }
+    //     }else if (c > a && c > b){
+    //         if (a > b) {
+    //             return console.log(c + " "+ a + " " + b)
+    //         }else {
+    //             return console.log(c + " " + b + " " + a)
+    //         }
+    //     }else {
+    //         return console.log("El valor no es valid")
+    //     }
+    // }
+    // console.log(ordenarMajorMenor(enter1,enter2,enter3))
 
-    function ordenarMajorMenor(a,b,c){
-        if (a > b && a > c){
-            if (b > c){
-                return console.log(a + " "+ b + " " + c)
-            }else {
-               return console.log(a + " " +  c + " "+ b)
-            }
-        }else if (b > a && b > c){
-            if (a > c){
-                return console.log(b + " " + a + " " + c )
-            }else {
-                return console.log(b + " " + c + " " + a)
-            }
-        }else if (c > a && c > b){
-            if (a > b) {
-                return console.log(c + " "+ a + " " + b)
-            }else {
-                return console.log(c + " " + b + " " + a)
-            }
+
+//8
+
+    // var numero = 6
+    //
+    // function parSenar ( numero ) {
+    //     for (let i = 0; i <= numero; i++) {
+    //         if (i % 2 == 0) {
+    //             console.log("El numero " + i + " Es parell")
+    //         } else {
+    //             console.log("EL numero " + i + " Es senar")
+    //         }
+    //     }
+    // }
+    //
+    // parSenar(numero)
+
+//9
+
+
+    // function patro () {
+    //
+    //     for (let i = 0; i < 5; i++){
+    //         if (i == 0){
+    //             console.log("*")
+    //         }else if( i == 1) {
+    //             console.log("* " + " *")
+    //         }else if ( i == 2) {
+    //             console.log("* " + " * " + " * ")
+    //         }else if ( i == 3) {
+    //             console.log("* " + " * " + " * " + " *")
+    //         }else {
+    //             console.log("* " + " * " + " * " + " * " + " *" )
+    //         }
+    //     }
+    // }
+    // patro ()
+
+//10
+
+
+function sumarMultiples () {
+    let numero = 0
+    let suma = 0;
+    do {
+        numero ++
+        if (numero % 3 === 0 || numero % 5 === 0) {
+            suma += numero;
+        }
+
+    } while (numero <= 1000)
+    return suma
+}
+
+console.log(sumarMultiples())
+
+//11
+
+function patróAsterics(linias) {
+    for (let i = 1; i <= linias; i++) {
+        if (i % 2 !== 0){
+        console.log("* ".repeat(i).trim());
+    }}
+}
+
+
+patróAsterics(5);
+
+
+//12
+
+
+let enter = 123456
+
+
+function intercambiarPosicio (numero){
+
+    //creo 2 variables una que seran els numeros que asigmare pasat a un string  i l'altre el resultat
+    let numeroAString = numero.toString()
+    let resultat = '';
+
+    for (let i = 0; i < numeroAString.length; i++) {
+        //el if per a mirar si son parells o senars i fer l'intercamib de posicio
+        if (i + 1 < numeroAString.length){
+            resultat += numeroAString[i + 1] + numeroAString[i]
+            i++
         }else {
-            return console.log("El valor no es valid")
+            resultat += numeroAString[i]
         }
     }
-    console.log(ordenarMajorMenor(enter1,enter2,enter3))
+    return resultat
+}
+console.log(intercambiarPosicio(enter))
